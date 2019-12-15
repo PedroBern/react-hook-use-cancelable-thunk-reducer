560744e58cce6e5b686ca75dbc58eb5daf828c10
Pedro Bern <pedrobermoreira@gmail.com>
2019-12-15 16:48:12 -0200

    Add tests

---
+++ test/index.test.jsx
@@ -0,0 +1,225 @@
+// eslint-disable-next-line
+import React from "react";
+import { mount } from "enzyme";
+import { act } from "react-dom/test-utils";
+import useCancelableThunkReducer from "../src/index";
+
+const myMock = jest.fn();
+const getStateMock = jest.fn(x => x);
+
+global.console = {
+  log: jest.fn()
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      myMock();
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+
+const incrementAsyncAction = () => async dispatch => {
+  // dispatch action after 1 second
+  setTimeout(() => {
+    dispatch({ type: "increment" });
+  }, 1000);
+};
+
+const incrementAsyncActionThunk = () => async (dispatch, getState) => {
+  const state = getState();
+  getStateMock(state);
+
+  // dispatch action after 1 second
+  setTimeout(() => {
+    dispatch({ type: "increment" });
+  }, 1000);
+};
+
+// eslint-disable-next-line react/prop-types
+const Counter = ({ initialState, init, verbose }) => {
+  const [state, dispatch] = useCancelableThunkReducer(
+    reducer,
+    initialState,
+    verbose,
+    init
+  );
+  return (
+    <div>
+      <p id="count">{state.count}</p>
+      <button id="increment" onClick={() => dispatch({ type: "increment" })}>
+        +
+      </button>
+      <button
+        id="incrementAsync"
+        onClick={() => incrementAsyncAction()(dispatch)}
+      >
+        + async
+      </button>
+      <button
+        id="incrementAsyncThunk"
+        onClick={() => dispatch(incrementAsyncActionThunk())}
+      >
+        + async thunk
+      </button>
+    </div>
+  );
+};
+
+const setupWrapper = ({
+  state = { count: 0 },
+  init = undefined,
+  verbose = false
+} = {}) => {
+  const wrapper = mount(
+    <Counter initialState={state} init={init} verbose={verbose} />
+  );
+  const count = wrapper.find("#count").first();
+  const increment = wrapper.find("#increment").first();
+  const incrementAsync = wrapper.find("#incrementAsync").first();
+  const incrementAsyncThunk = wrapper.find("#incrementAsyncThunk").first();
+
+  return {
+    wrapper,
+    count,
+    increment,
+    incrementAsync,
+    incrementAsyncThunk
+  };
+};
+
+jest.useFakeTimers();
+
+describe("useCancelableThunkReducer", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.clearAllTimers();
+  });
+
+  it("dispatch actions", () => {
+    const { wrapper, count, increment } = setupWrapper();
+    expect(count.text()).toBe("0");
+    increment.simulate("click", { button: 0 });
+    wrapper.update();
+    expect(count.text()).toBe("1");
+  });
+
+  it("cancel actions when unmounted", () => {
+    const { wrapper, count, incrementAsync } = setupWrapper({ verbose: true });
+
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+    expect(myMock.mock.calls.length).toBe(0);
+    expect(count.text()).toBe("0");
+
+    incrementAsync.simulate("click", { button: 0 });
+    act(() => {
+      jest.runOnlyPendingTimers();
+    });
+    wrapper.update();
+    expect(setTimeout).toHaveBeenCalledTimes(2);
+    expect(myMock.mock.calls.length).toBe(1);
+    expect(count.text()).toBe("1");
+
+    incrementAsync.simulate("click", { button: 0 });
+    wrapper.unmount();
+    act(() => {
+      jest.runOnlyPendingTimers();
+    });
+
+    expect(global.console.log).toHaveBeenCalledWith("Canceled: ", "increment");
+
+    // setTimeout called one more time,
+    expect(setTimeout).toHaveBeenCalledTimes(3);
+    // but dispatch was not called, as expected
+    expect(myMock.mock.calls.length).toBe(1);
+  });
+
+  it("cancel thunk actions when unmounted", () => {
+    const { wrapper, count, incrementAsyncThunk } = setupWrapper({
+      verbose: true
+    });
+
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+    expect(myMock.mock.calls.length).toBe(0);
+    expect(count.text()).toBe("0");
+
+    incrementAsyncThunk.simulate("click", { button: 0 });
+    act(() => {
+      jest.runOnlyPendingTimers();
+    });
+    wrapper.update();
+    expect(setTimeout).toHaveBeenCalledTimes(2);
+    expect(myMock.mock.calls.length).toBe(1);
+    expect(count.text()).toBe("1");
+
+    incrementAsyncThunk.simulate("click", { button: 0 });
+    wrapper.unmount();
+    act(() => {
+      jest.runOnlyPendingTimers();
+    });
+
+    expect(global.console.log).toHaveBeenCalledWith("Canceled: ", "increment");
+
+    // setTimeout called one more time,
+    expect(setTimeout).toHaveBeenCalledTimes(3);
+    // but dispatch was not called, as expected
+    expect(myMock.mock.calls.length).toBe(1);
+  });
+
+  it("console.log on verbose", () => {
+    const { wrapper, incrementAsync } = setupWrapper({ verbose: true });
+
+    incrementAsync.simulate("click", { button: 0 });
+    wrapper.unmount();
+    act(() => {
+      jest.runOnlyPendingTimers();
+    });
+    expect(global.console.log).toHaveBeenCalledWith("Canceled: ", "increment");
+  });
+
+  it("pass right init function", () => {
+    function init(initialCount) {
+      myMock();
+      return { count: initialCount };
+    }
+
+    const initialCount = 0;
+
+    setupWrapper({ state: initialCount, init });
+    expect(myMock.mock.calls.length).toBe(1);
+  });
+
+  it("dispatch thunks", () => {
+    const { incrementAsyncThunk } = setupWrapper();
+    incrementAsyncThunk.simulate("click", { button: 0 });
+    expect(getStateMock.mock.calls.length).toBe(1);
+    expect(getStateMock.mock.calls[0][0]).toStrictEqual({ count: 0 });
+  });
+
+  it("getState returns current state", () => {
+    const { wrapper, incrementAsyncThunk } = setupWrapper();
+    incrementAsyncThunk.simulate("click", { button: 0 });
+    expect(getStateMock.mock.calls.length).toBe(1);
+    expect(getStateMock.mock.calls[0][0]).toStrictEqual({ count: 0 });
+    act(() => {
+      jest.runOnlyPendingTimers();
+    });
+    wrapper.update();
+    incrementAsyncThunk.simulate("click", { button: 0 });
+    expect(getStateMock.mock.calls.length).toBe(2);
+    expect(getStateMock.mock.calls[1][0]).toStrictEqual({ count: 1 });
+  });
+
+  it("don't console.log if not verbose", () => {
+    const wrapper = mount(<Counter initialState={{ count: 0 }} />);
+    const incrementAsync = wrapper.find("#incrementAsync").first();
+    incrementAsync.simulate("click", { button: 0 });
+    wrapper.unmount();
+    act(() => {
+      jest.runOnlyPendingTimers();
+    });
+    expect(global.console.log).not.toHaveBeenCalled();
+  });
+});
