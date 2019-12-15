5b95fab7400b2f0a5514f9e9492f7ea740859063
Pedro Bern <pedrobermoreira@gmail.com>
2019-12-15 17:11:55 -0200

    fix typo

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,35 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 const path = require("path");
 
 const { NODE_ENV } = process.env;
 
 const plugins = [
   new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
-  }),
+    "process.env.NODE_ENV": JSON.stringify(NODE_ENV)
+  })
 ];
 
-const filename = `useAbortableReducer${NODE_ENV === 'production' ? '.min' : ''}.js`;
+const filename = `use-cancelable-thunk-reducer{NODE_ENV === 'production' ? '.min' : ''}.js`;
 
 module.exports = {
-  mode: NODE_ENV === 'production' ? 'production' : 'development',
+  mode: NODE_ENV === "production" ? "production" : "development",
 
   module: {
-    rules: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-    ],
+    rules: [{ test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ }]
   },
 
-  entry: [
-    './src/index',
-  ],
+  entry: ["./src/index"],
 
   optimization: {
-    minimize: NODE_ENV === 'production',
+    minimize: NODE_ENV === "production"
   },
 
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, "dist"),
     filename,
-    library: 'useAbortableReducer',
-    libraryTarget: 'umd',
+    library: "useCancelableThunkReducer",
+    libraryTarget: "umd"
   },
 
-  plugins,
+  plugins
 };
